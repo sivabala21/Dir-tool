d621a21095202c7e5c922ab640b53544a3683c64
Siva <56733505+BOT-CODER@users.noreply.github.com>
2019-10-18 22:29:34 +0530

    Script to run Dirpy file

---
+++ DIR.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#
+#
+apt update -y && apt upgrade -y
+apt install python python2 git curl
+cd
+echo "          Download"
+git clone https://github.com/BOT-CODER/PY_CODER.git
+cd PY_CODER/
+echo "          Runing..."
+python Dirpy.py
+#
+#
